bc8b4de77474041a7e9228b4039a46d159edf448
Burak Sezer <burak.sezer.developer@gmail.com>
2011-03-20 16:36:56 +0000

    clear() and reset() removed

--- src/interpreter.py
+++ src/interpreter.py
@@ -179,6 +179,5 @@ def run(script, env):
             traceback.print_exc(err)
             out.error("an error occurred when running the %s function." % out.color(opr, "red"))
             lpms.terminate()
-    utils.xterm_title("lpms: %s/%s finished" % (ipr.env.category, ipr.env.pkgname))
-    utils.xterm_title_reset()
-    ipr.env.__dict__.clear()
+    #utils.xterm_title_reset()
+    #ipr.env.__dict__.clear()
