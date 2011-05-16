c93770c902f9c1a0fdf502bab6c451bb8f729ac3
Burak Sezer <bburaksezer@gmail.com>
2011-05-16 20:46:31 +0300

    fix compiling problems

--- src/internals.py
+++ src/internals.py
@@ -15,10 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with lpms.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
+from lpms import constants as cst 
+
 class Environment(object):
-    def __init__(self):
-        pass
-        #self.__dict__["pkgname"] = pkgname
+    pass
 
 class InternalFuncs(object):
     def __init__(self):
@@ -28,6 +30,8 @@ class InternalFuncs(object):
         self.env.backup = []
         # FIXME: use a better method for environment functions.
         self.env.__dict__['get'] = self.get
+        for f in cst.environmental_files:
+            self.import_script(os.path.join(cst.lpms_path, f))
 
     def import_script(self, script_path):
         exec compile(open(script_path).read(), "error", "exec") in self.env.__dict__
