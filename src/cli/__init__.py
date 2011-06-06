d6e9a016ee8b43b3fb18b9713e551c97e298d2d1
Burak Sezer <bburaksezer@gmail.com>
2011-06-06 08:53:53 -0700

    new cli, ungodly hack

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -16,31 +16,31 @@
 # along with lpms.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+
 import lpms
 
-from lpms.operations import build
+from lpms import api
+from lpms import out
+from lpms import conf
+from lpms import utils
+
 from lpms.operations import remove
 from lpms.operations import update
-from lpms import utils
-from lpms import conf
-from lpms import out
-from lpms import api
 
-lpms_version = "0.9_alpha1"
+cli = sys.argv[1:]
 
-help_output = (
-        ('--help', '-h', 'Shows this message.'),
+lpms_version = '0.9_alpha2'
+
+help_output = (('--help', '-h', 'Shows this message.'),
         ('--version', '-v', 'Shows version.'),
         ('--no-color', '-n', 'Disables color output.'),
         ('--remove', '-r', 'Removes given package.'),
         ('--update', '-u', 'Updates all repositories or given repository.'),
         ('--search', '-s', 'Searches given package in database.'),
         ('--belong', '-b', 'Queries the package that owns given keyword.'),
-        ('--list-files', '-lf', 'Lists files of given package.')
-        )
+        ('--content', '-c', 'Lists files of given package.'))
 
-build_help = (
-        ('--pretend', '-p', 'Shows operation steps'),
+build_help = (('--pretend', '-p', 'Shows operation steps'),
         ('--ask', '-a', 'Asks to the user before operation.'),
         ('--fetch-only', '-F', 'Only fetches packages, do not install.(not yet)'),
         ('--search', '-s', 'Searches given keyword in database.'),
@@ -53,121 +53,171 @@ build_help = (
         ('--resume-build', 'Resumes the most recent build operation.'),
         ('--change-root', 'Changes installation target.'),
         ('--no-merge', 'Does not merge the package'),
+        ('--ask-repo', 'Shows repo selection dialog if necessary.'),
         ('--show-opts', 'Shows available options for given packages'),
         ('--opts', 'Determines options of the package.'))
 
 def version():
-    out.write("lpms %s\n" % lpms_version)
+    out.write(('lpms %s\n' % lpms_version))
     lpms.terminate()
 
-def help():
-    out.normal("lpms -- %s Package Management System on %s" % (out.color("L", "red"), conf.LPMSConfig().distribution))
-    out.write("\nIn order to build a package:\n")
-    out.write(" # lpms <package-name> <extra-command>\n\n")
-    out.write("Build related commands:\n")
+
+
+def usage():
+    out.normal(('lpms -- %s Package Management System on %s' % (out.color('L', 'red'), conf.LPMSConfig().distribution)))
+    out.write('\nIn order to build a package:\n')
+    out.write(' # lpms <package-name> <extra-command>\n\n')
+    out.write('Build related commands:\n')
     for cmd in build_help:
-        if len(cmd) == 3:
-            out.write("%-27s %-10s : %s\n" % (out.color(cmd[0], "green"), out.color(cmd[1], "green"), cmd[2]))
+        if (len(cmd) == 3):
+            out.write(('%-27s %-10s : %s\n' % (out.color(cmd[0], 'green'),
+             out.color(cmd[1], 'green'),
+             cmd[2])))
         else:
-            out.write("%-30s : %s\n" % (out.color(cmd[0], "green"), cmd[1]))
+            out.write(('%-30s : %s\n' % (out.color(cmd[0], 'green'), cmd[1])))
 
-    out.write("\nOther Commands:\n")
+    out.write('\nOther Commands:\n')
     for cmd in help_output:
-        out.write("%-27s %-10s : %s\n" % (out.color(cmd[0], "green"), out.color(cmd[1], "green"), cmd[2]))
+        out.write(('%-27s %-10s : %s\n' % (out.color(cmd[0], 'green'),
+         out.color(cmd[1], 'green'),
+         cmd[2])))
+
     lpms.terminate()
+
+
+exceptions = ('change-root', 'opts')
+
+toinstruct = ('ask', 'a', 'resume-build', 'resume', 'pretend', 'p', 'fetch-only', 'F', \
+        'no-merge', 'U', 'remove', 'r', 'upgrade', 'skip-first')
+
+regular = ('help', 'h', 'version', 'v', 'belong', 'b', 'content', 'c', 'remove', 'r', \
+        'no-color', 'n', 'update', 'u', 'search', 's', 'upgrade', 'U', 'ask-repo')
+
+instruct = {'ask': False, 'pretend': False, 'resume-build': False, 'resume': False, \
+        'pretend': False, 'no-merge': False, 'fetch-only': False, 'real_root': None, \
+        'cmd_options': [], 'ignore-deps': False, 'sandbox': None,'stage': None, \
+        'force': None, 'upgrade': None, 'remove': None, 'skip-first': False}
 
 def main():
-    command_line = sys.argv
-    options = command_line[1:]
-    pkgnames = []; ecoms = ('--resume-build')
-    instruct = {"cmd_options": [], "sandbox": None, "show_opts": None, "ask": False, "resume": False,
-            "skip_first": False, "pretend": False, "stage": None, "force": None, "real_root": None, 
-            "ignore-deps": False}
+    packages = []; invalid = []
+    for l in cli:
+        if (l.startswith('-') and (not l.startswith('--'))):
+            for h in l[1:]:
+                if (h == 'h'):
+                    usage()
+                elif (h == 'v'):
+                    version()
+                elif (h == 'b'):
+                    from lpms.cli import belong
+                    belong.main(cli[(cli.index(l) + 1):])
+                    return
+                elif (h == 'i'):
+                    from lpms.cli import info
+                    info.Info(cli[(cli.index(l) + 1):]).run()
+                    return
+                elif (h == 'c'):
+                    from lpms.cli import list_files
+                    try:
+                        list_files.main(cli[(cli.index(l) + 1)])
+                        return
+                    except IndexError:
+                        out.error('please give a package name.')
+                        lpms.terminate()
+                elif (h == 's'):
+                    from lpms.cli import search
+                    search.Search(cli[(cli.index(l) + 1):]).search()
+                    return
+                elif (h == 'u'):
+                    utils.check_root()
+                    try:
+                        update.main(cli[(cli.index(l) + 1):])
+                    except IndexError:
+                        update.main()
+                    return
+                else:
+                    if ((h not in regular) and (h not in toinstruct)):
+                        invalid.append(('-' + h))
 
-    for opt in options:
-        if opt in ecoms:
-            continue
-        elif opt == "-h" or opt == "--help":
-            help()
-        elif opt == "-v" or opt == "--version":
-            version()
-        elif opt == "--update" or  opt == "-u":
-            utils.check_root()
-            try:
-                update.main(options[options.index(opt)+1:])
-            except IndexError:
-                update.main()
-            return
-        elif opt == "--remove" or opt == "-r":
-            continue
-        elif opt == "--force" or opt == "-f":
-            instruct["force"] = True
-        elif opt == "--show-opts":
-            instruct["show_opts"] = True
-        elif opt.startswith("--opts"):
-            instruct["cmd_options"] = opt.split("=")[1].strip().split(" ")
-        elif opt == "--ignore-deps":
-            instruct["ignore-deps"] = True
-        elif opt.startswith("--stage"):
-            instruct["stage"] = opt.split("=")[1]
-        elif opt == "--disable-sandbox":
-            instruct["sandbox"] = False
-        elif opt == "--enable-sandbox":
-            instruct["sandbox"] = True
-        elif opt == "--pretend" or opt == "-p":
-            instruct["pretend"] = True
-        elif opt == "--ask" or opt == "-a":
-            instruct["ask"] = True
-        elif opt == "--search" or opt == "-s":
-            from lpms.cli import search
-            search.Search(options[options.index(opt)+1:]).search()
-            return
-        elif opt == "--list-files" or opt == "-lf":
-            from lpms.cli import list_files
-            try: list_files.main(options[options.index(opt)+1])
-            except IndexError:
-                out.error("please give a package name.")
-                lpms.terminate()
-            return
-        elif opt == "--belong" or opt == "-b":
-            from lpms.cli import belong
-            belong.main(options[options.index(opt)+1])
-            return
-        elif opt == "--remove_repo" or opt == "-rr":
-            from lpms.cli import remove_repo
-            try: remove_repo.main(options[options.index(opt)+1:])
-            except IndexError:
-                out.error("please give a repo name.")
-                lpms.terminate()
-            return
-        elif opt == "--no-merge":
-            pass
-        elif opt.startswith("--change-root"):
-            instruct["real_root"] = opt.split("=")[1].strip()
-        elif opt == "--info" or opt == "-i":
-            from lpms.cli import info
-            info.Info(options[options.index(opt)+1:]).run()
-            return
-        elif opt == "--skip-first":
-            instruct["skip_first"] = True
+        elif l.startswith('--'):
+            if (l[2:] == 'help'):
+                usage()
+            elif (l[2:] == 'version'):
+                version()
+            elif (l[2:] == 'update'):
+                utils.check_root()
+                try:
+                    update.main(cli[(cli.index(l) + 1):])
+                except IndexError:
+                    update.main()
+                return
+            elif (l[2:] == 'info'):
+                from lpms.cli import info
+                info.Info(cli[(cli.index(l) + 1):]).run()
+                return
+            elif (l[2:] == 'belong'):
+                from lpms.cli import belong
+                belong.main(cli[(cli.index(l) + 1):])
+                return
+            elif (l[2:] == 'content'):
+                from lpms.cli import list_files
+                try:
+                    list_files.main(cli[(cli.index(l) + 1)])
+                    return
+                except IndexError:
+                    out.error('please give a package name.')
+                    lpms.terminate()
+            elif (l[2:] == 'search'):
+                from lpms.cli import search
+                search.Search(cli[(cli.index(l) + 1):]).search()
+                return
+            else:
+                if (((l[2:] not in regular) and (l[2:] not in toinstruct)) and \
+                        (l[2:].split('=')[0] not in exceptions)):
+                    invalid.append(l)
         else:
-            pkgnames.append(opt)
+            packages.append(l)
 
-    if "--upgrade" in options or "-U" in options:
-        api.upgrade_system(instruct)
-        return
+    for c in cli:
+        if (c.startswith('-') and (not c.startswith('--'))):
+            for x in c[1:]:
+                if (x in toinstruct):
+                    instruct[toinstruct[(toinstruct.index(x) - 1)]] = True
 
-    # abort lpms if the user do not give package name
-    if not "--resume" in options and not pkgnames:
-        lpms.catch_error("please give a package name!")
+        else:
+            if ((c[2:] in toinstruct) or (c[2:].split('=')[0] in exceptions)):
+                if c[2:].startswith('change-root'):
+                    instruct['real_root'] = c[2:].split('=')[1]
+                if c[2:].startswith('opts'):
+                    instruct['cmd_options'] = c[2:].split('=')[1].split(' ')
+                if c[2:].startswith('stage'):
+                    instruct['stage'] = opt.split('=')[1].strip()
+                instruct[c[2:]] = True
 
-    if "--remove" in options or "-r" in options:
-        remove.main(pkgnames, instruct)
-        return
-    
-    if "--resume" in options:
-        instruct["resume"] = True
+    if invalid:
+        out.warn("invalid: "+", ".join(invalid))
+
+    if instruct['resume']:
         pkgnames = []
-    # build given packages
+
+    if instruct['upgrade']:
+        api.upgrade_system(instruct)
+        
+    if instruct['remove']:
+        remove.main(packages, instruct)
+        return
+
+    for tag in ('upgrade', 'remove'):
+        del instruct[tag]
+
+    if not packages and not instruct["resume"]:
+        out.error('no given package name.')
+        lpms.terminate()
+
+    if instruct["resume"]:
+        packages = []
+
     utils.check_root()
-    api.pkgbuild(pkgnames, instruct)
+    #lpms.logger.info(('=== command: %s ===' % ' '.join(sys.argv)))
+
+    api.pkgbuild(packages, instruct)
+
