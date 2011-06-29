e2c8d123ea828f36771e65be4daa55677be1936a
Burak Sezer <bburaksezer@gmail.com>
2011-06-29 15:57:24 -0700

    intltoolize func and doc strings added

--- src/buildtools.py
+++ src/buildtools.py
@@ -30,15 +30,19 @@ def standard_setup(*parameters):
     pass
 
 def standard_configure(*parameters):
+    '''Runs standard configuration function'''
     return conf(*parameters)
 
 def standard_install(parameters='', arg='install'):
+    '''Runs standard installation function'''
     return linstall(parameters='', arg='install')
 
 def standard_build(*parameters):
+    '''Runs standard build function'''
     return make(*parameters)
 
 def conf(*args, **kwargs):
+    '''Runs configure script with standard and given parameters'''
     conf_command = './configure'
     if "run_dir" in kwargs:
         conf_command = os.path.join(kwargs["run_dir"], "configure")
@@ -68,10 +72,12 @@ def conf(*args, **kwargs):
         out.warn("no configure script found.")
 
 def raw_configure(*parameters):
+    '''Runs configure script with only given parameters'''
     if not shelltools.system("./configure %s" % " ".join(parameters)):
         lpms.terminate()
 
 def make(*parameters, **kwargs):
+    '''Runs standard build command with given parameters'''
     if "j" in kwargs:
         jobs = "-j"+str(kwargs["j"])
     else:
@@ -81,10 +87,12 @@ def make(*parameters, **kwargs):
         lpms.catch_error("make failed.")
 
 def raw_install(parameters = '', arg='install'):
+    '''Runs installation function with only given parameters'''
     if not shelltools.system("make %s %s" % (parameters, arg)):
         lpms.catch_error("raw_install() function failed.")
 
 def linstall(parameters='', arg='install'):
+    '''Runs standard installation function with given parameters and commands'''
     args = 'make prefix=%(prefix)s/%(defaultprefix)s \
             datadir=%(prefix)s/%(data)s \
             infodir=%(prefix)s/%(info)s \
@@ -109,26 +117,37 @@ def linstall(parameters='', arg='install'):
         lpms.catch_error("listall failed.")
 
 def aclocal(*parameters):
+    '''Runs aclocal with given parameters'''
     if not shelltools.system("aclocal %s" % " ".join(parameters)):
         lpms.catch_error("aclocal failed.")
 
+def intltoolize(*parameters):
+    '''Runs intltoolize with given parameters'''
+    if not shelltools.system("intltoolize %s" % " ".join(parameters)):
+        lpms.catch_error("intltoolize failed.")
+
 def libtoolize(*parameters):
+    '''Runs libtoolize with given parameters'''
     if not shelltools.system("libtoolize %s" % " ".join(parameters)):
         lpms.catch_error("libtoolize failed.")
 
 def autoconf(*parameters):
+    '''Runs autoconf with given parameters'''
     if not shelltools.system("autoconf %s" % " ".join(parameters)):
         lpms.catch_error("autoconf failed.")
 
 def autoreconf(*parameters):
+    '''Runs autoreconf with given parameters'''
     if not shelltools.system("autoreconf %s" % " ".join(parameters)):
         lpms.catch_error("autoreconf failed.")
 
 def automake(*parameters):
+    '''Runs automake with given parameters'''
     if not shelltools.system("automake %s" % " ".join(parameters)):
         lpms.catch_error("automake failed.")
 
 def autoheader(*parameters):
+    '''Runs autoheader with given parameters'''
     if not shelltools.system("autoheader %s" % " ".join(parameters)):
         lpms.catch_error("autoheader failed.")
 
