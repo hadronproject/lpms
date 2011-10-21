958c5ff1a37b039ce14c04a812f709814f0104db
Burak Sezer <bburaksezer@gmail.com>
2011-10-21 15:12:38 -0700

    changes for news feature

--- src/news.py
+++ src/news.py
@@ -15,8 +15,37 @@
 # You should have received a copy of the GNU General Public License
 # along with lpms.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
+from lpms import out
 from lpms import utils
+from lpms import constants as cst
 
-keys = ('from', 'summary', 'date', 'priority')
+from collections import OrderedDict
 
+metadata_keys = ('from', 'summary', 'date', 'priority')
 
+class News(object):
+    def __init__(self):
+        self.valid_repos = utils.valid_repos()
+        self.data = []
+
+    def import_repo_news(self, repo):
+        '''Imports news of given repository'''
+        my_news_dir = os.path.join(cst.repos, repo, cst.news_dir)
+        if not os.path.isdir(my_news_dir):
+            return
+        
+        for news in os.listdir(my_news_dir):
+            local = utils.import_script(os.path.join(my_news_dir, news))
+            try:
+                metadata = utils.metadata_parser(local["metadata"], keys=metadata_keys)
+            except IndexError:
+                out.warn("Syntax errors found in...")
+                continue
+            self.data.append((repo, metadata, local["message"]))
+
+    def get_all_news(self):
+        '''Collects available news'''
+        for repo in self.valid_repos:
+            self.import_repo_news(repo)
