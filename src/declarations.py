b5af2829e1aa9e5225d42d44a8911c9e9fc3ee77
Burak Sezer <burak.sezer.developer@gmail.com>
2011-11-21 14:08:19 -0800

    some improvments in Value class and ConstError moved to exceptions module

--- src/declarations.py
+++ src/declarations.py
@@ -19,23 +19,21 @@ import sys
 import glob
 
 from lpms import singleton
+from lpms import exceptions
 
 # Based on PiSi and a Stack Overflow entry in this part
 # http://stackoverflow.com/questions/31875/is-there-a-simple-elegant-way-to-define-singletons-in-python/33201#33201
 
-class Value:
-    class ConstError(TypeError):
-        pass
-    
+class Value:   
     def __setattr__(self, name, value):
-        if self.__dict__.has_key(name):
-            raise self.ConstError, "can't rebind constant: %s" % name
+        if name in self.__dict__:
+            raise exceptions.ConstError, "can't rebind constant: %s" % name
         # Binding an attribute once to a const is available
         self.__dict__[name] = value
         
     def __delattr__(self, name):
-        if self.__dict__.has_key(name):
-            raise self.ConstError, "Can't unbind constant: %s" % name
+        if name in self.__dict__:
+            raise exceptions.ConstError, "can't unbind constant: %s" % name
         # we don't have an attribute by this name
         raise NameError, name
 
