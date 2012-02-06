f998e0ed93a4a210160d47c83d33905964ee7875
Burak Sezer <bburaksezer@gmail.com>
2012-02-06 13:33:57 -0800

    version bump -> 1.0.2, new bugfix release of 1.0 series

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -33,7 +33,7 @@ from lpms.operations import update
 
 commands = sys.argv[1:]
 
-lpms_version = '1.0.1'
+lpms_version = '1.0.2'
 
 help_output = (('--help', '-h', 'Shows this message.'),
         ('--version', '-v', 'Shows version.'),
