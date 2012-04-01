4ac95f434cce06dd7ab0ce24ed9504ae15da4c5d
Burak Sezer <bburaksezer@gmail.com>
2012-04-01 22:25:25 +0300

    db vol.2, a new and orm style database module introduced

---
+++ src/db/api/__init__.py
@@ -0,0 +1,263 @@
+# Copyright 2009 - 2012 Burak Sezer <purak@hadronproject.org>
+# 
+# This file is part of lpms
+#  
+# lpms is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#   
+# lpms is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#   
+# You should have received a copy of the GNU General Public License
+# along with lpms.  If not, see <http://www.gnu.org/licenses/>.
+
+# Database access api for lpms and related applications
+
+import cPickle as pickle
+
+from lpms.exceptions import DatabaseAPIError
+
+from lpms.types import LCollect
+from lpms.types import PackageItem
+
+from lpms.db import installdb
+from lpms.db import repositorydb
+
+class RepositoryDB:
+    def __init__(self):
+        self.database = repositorydb.RepositoryDatabase()
+        
+    def insert_package(self, dataset, commit=False):
+        self.database.insert_package(dataset, commit)
+
+    def find_package(self, **kwargs):
+        results = PackageItem()
+        added_packages = []
+        object_items = ('id', 'repo', 'category', 'name', 'version', 'slot', 'arch', 'options', \
+                        'optional_depends_build', 'optional_depends_runtime', 'optional_depends_postmerge', \
+                        'optional_depends_conflict', 'static_depends_build', 'static_depends_runtime')
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        p_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+
+        # Get the package query
+        package_query = self.database.find_package(package_id=p_id, package_repo=repo, \
+                package_category=category, package_name=name, package_version=version)
+        
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        # Add the packages to the object
+        for package in package_query:
+            # [0] => repo, [1] => category [2] => name, [3] => version, [6] => arch
+            if not (package[1], package[2], package[3], package[4], package[6]) in added_packages:
+                added_packages.append((package[1], package[2], package[3], package[4], package[6]))
+            else: continue
+            for item in object_items:
+                index = object_items.index(item)
+                if index >= 7:
+                    setattr(pkg_obj, item, pickle.loads(str(package[index])))
+                    continue
+                setattr(pkg_obj, item, package[index])
+            results.add(pkg_obj)
+        return results
+
+    def get_package_metadata(self, **kwargs):
+        object_items = ('id', 'repo', 'category', 'name', 'version', 'slot', 'summary', \
+                'homepage', 'license', 'src_uri', 'arch', 'options')
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        p_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+
+        package_query = self.database.get_package_metadata(package_id=p_id, package_repo=repo, \
+                package_category=category, package_name=name, package_version=version)
+        
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        # Add the packages to the object
+        for item in object_items:
+            index = object_items.index(item)
+            if index == 11:
+                setattr(pkg_obj, item, pickle.loads(str(package_query[index])))
+                continue
+            setattr(pkg_obj, item, package_query[index])
+        return pkg_obj
+
+    def get_package_dependencies(self, package_id):
+        object_items = ('optional_depends_build', 'optional_depends_runtime', 'optional_depends_postmerge', \
+                'optional_depends_conflict', 'static_depends_build', 'static_depends_runtime')
+        package_query = self.database.get_package_dependencies(package_id)
+        
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        for item in object_items:
+            index = object_items.index(item)
+            setattr(pkg_obj, item, pickle.loads(str(package_query[index])))
+        return pkg_obj
+
+
+    def delete_package(self, **kwargs):
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        package_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+        package_commit = kwargs.get("commit", False)
+        self.repositorydb.delete_package(package_id=p_id, package_repo=repo, package_category=category, \
+                package_name=name, package_version=version, commit=package_commit)
+
+    def delete_repository(self, repo, commit=False):
+        self.repositorydb.delete_repository(repo, commit)
+
+class InstallDB:
+    def __init__(self):
+        self.database = installdb.InstallDatabase()
+        
+    def insert_package(self, dataset, commit=False):
+        self.database.insert_package(dataset, commit)
+
+    def find_package(self, **kwargs):
+        results = PackageItem()
+        added_packages = []
+        object_items = ('id', 'repo', 'category', 'name', 'version', 'slot', 'arch', 'options', \
+                        'optional_depends_build', 'optional_depends_runtime', 'optional_depends_postmerge', \
+                        'optional_depends_conflict', 'static_depends_build', 'static_depends_runtime', \
+                        'static_depends_postmerge', 'static_depends_conflict', \
+                        'optional_reverse_build', 'optional_reverse_runtime', 'optional_reverse_postmerge', \
+                        'optional_reverse_conflict', 'static_reverse_build', 'static_reverse_runtime', 'static_reverse_postmerge', 'static_reverse_conflict')
+
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        if name is None:
+            raise DatabaseAPIError("you must give package_name parameter")
+        p_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+        
+        # Get the package query
+        package_query = self.database.find_package(package_id=p_id, package_repo=repo, \
+                package_category=category, package_name=name, package_version=version)
+
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        # Add the packages to the object
+        for package in package_query:
+            # [0] => repo, [1] => category [2] => name, [3] => version, [6] => arch
+            if not (package[1], package[2], package[3], package[4], package[6]) in added_packages:
+                added_packages.append((package[1], package[2], package[3], package[4], package[6]))
+            else: continue
+            for item in object_items:
+                index = object_items.index(item)
+                if index >= 7:
+                    setattr(pkg_obj, item, pickle.loads(str(package[index])))
+                    continue
+                setattr(pkg_obj, item, package[index])
+            results.add(pkg_obj)
+        return results
+
+    def get_package_metadata(self, **kwargs):
+        object_items = ('id', 'repo', 'category', 'name', 'version', 'slot', 'summary', \
+                'homepage', 'license', 'src_uri', 'arch', 'options')
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        p_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+
+        package_query = self.database.get_package_metadata(package_id=p_id, package_repo=repo, \
+                package_category=category, package_name=name, package_version=version)
+        
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        # Add the packages to the object
+        for item in object_items:
+            index = object_items.index(item)
+            if index == 11:
+                setattr(pkg_obj, item, pickle.loads(str(package_query[index])))
+                continue
+            setattr(pkg_obj, item, package_query[index])
+        return pkg_obj
+
+    def get_package_dependencies(self, package_id):
+        object_items = ('optional_depends_build', 'optional_depends_runtime', 'optional_depends_postmerge', \
+                'optional_depends_conflict', 'static_depends_build', 'static_depends_runtime', \
+                'static_depends_postmerge', 'static_depends_conflict', 'optional_reverse_build', \
+                'optional_reverse_runtime', 'optional_reverse_postmerge', \
+                'optional_reverse_conflict', 'static_reverse_build', 'static_reverse_runtime', \
+                'static_reverse_postmerge', 'static_reverse_conflict')
+        package_query = self.database.get_package_dependencies(package_id)
+        
+        # Create a LCollect object
+        pkg_obj = LCollect()
+
+        for item in object_items:
+            index = object_items.index(item)
+            setattr(pkg_obj, item, pickle.loads(str(package_query[index])))
+        return pkg_obj
+
+
+    def delete_package(self, **kwargs):
+        # Set the keywords
+        name = kwargs.get("package_name", None)
+        package_id = kwargs.get("package_id", None)
+        repo = kwargs.get("package_repo", None)
+        category = kwargs.get("package_category", None)
+        version = kwargs.get("package_version", None)
+        package_commit = kwargs.get("commit", False)
+        self.repositorydb.delete_package(package_id=p_id, package_repo=repo, package_category=category, \
+                package_name=name, package_version=version, commit=package_commit)
+
+    def delete_repository(self, repo, commit=False):
+        self.repositorydb.delete_repository(repo, commit)
+
+# For testing purposes
+"""
+a = InstallDB()
+
+dataset = LCollect()
+dataset.repo = "main"
+dataset.category = "app-editors"
+dataset.name = "nano"
+dataset.version = "2.2.6"
+dataset.slot = "0"
+dataset.summary = "Pico editor clone with enhancements"
+dataset.homepage = "http://www.nano-editor.org"
+dataset.license = "GPL-2"
+dataset.src_uri = "mirror:/hadronproject/nano-2.2.6.tar.gz"
+dataset.options = ["X", "gtk", "nls", "curses"]
+dataset.arch = "x86"
+dataset.optional_depends_build = { 'nls': ["sys-libs/ncurses"] }
+dataset.optional_depends_runtime = { 'nls': ["sys-libs/ncurses"] }
+dataset.optional_depends_postmerge = {}
+dataset.optional_depends_conflict = {}
+dataset.static_depends_build = ["sys-libs/glibc"]
+dataset.static_depends_runtime = ["sys-libs/glibc"]
+dataset.static_depends_postmerge = []
+dataset.static_depends_conflict = []
+
+dataset.optional_reverse_build = { 'nls': ["sys-libs/ncurses"] }
+dataset.optional_reverse_runtime = { 'nls': ["sys-libs/ncurses"] }
+dataset.optional_reverse_postmerge = {}
+dataset.optional_reverse_conflict = {}
+dataset.static_reverse_build = ["sys-libs/glibc"]
+dataset.static_reverse_runtime = ["sys-libs/glibc"]
+dataset.static_reverse_postmerge = []
+dataset.static_reverse_conflict = []
+"""
--- src/db/db.py
+++ src/db/db.py
@@ -1,4 +1,4 @@
-# Copyright 2009 - 2011 Burak Sezer <purak@hadronproject.org>
+# Copyright 2009 - 2012 Burak Sezer <purak@hadronproject.org>
 # 
 # This file is part of lpms
 #  
@@ -16,294 +16,47 @@
 # along with lpms.  If not, see <http://www.gnu.org/licenses/>.
 
 import sqlite3
-import cPickle as pickle
 
 import lpms
-from lpms.db import skel
-from lpms.exceptions import ItemNotFound
+from lpms.db import schemas
 
-class LCollect(object):
-    def __init__(self, **kwargs):
-        for kwarg in kwargs:
-            setattr(self, kwarg, \
-                    kwargs[kwarg].decode("utf-8"))
-            
-    def __setattr__(self, item, value):
-        self.__dict__[item] = value.decode('utf-8')
-        
-    def __getattr__(self, item):
-        if not item in self.__dict__:
-            raise ItemNotFound("%s not found in LCollect object." % item)
-        return self.__dict__[item]
-    
-    def __delattr__(self, item):
-        if not item in self.__dict__:
-            raise ItemNotFound("%s not found in LCollect object." % item)
-        del self.__dict__[item]
-
-
-class PackageDatabase:
+class PackageDatabase(object):
     def __init__(self, db_path):
         self.db_path = db_path
         try:
             self.connection = sqlite3.connect(self.db_path)
         except sqlite3.OperationalError:
+            # TODO: Use an exception for this.
             lpms.terminate("lpms could not connected to the database (%s)" % self.db_path)
 
         self.cursor = self.connection.cursor()
-        table = self.cursor.execute('select * from sqlite_master where type = "table"')
+        table = self.cursor.execute('SELECT * FROM sqlite_master WHERE type = "table"')
         if table.fetchone() is None:
             self.initialize_db()
 
     def initialize_db(self):
-        tablelist = self.cursor.execute('select * from sqlite_master where type="table"')
-        tablelist = self.cursor.fetchall()
+        self.cursor.execute('SELECT * FROM sqlite_master WHERE type = "table"')
+        tables = self.cursor.fetchall()
         content = []
-        for i in tablelist:
-            content += list(i)
+        for table in tables:
+            content.extent(list(table))
 
-        # get list of tables and drop them
-        for t in content:
+        if not content:
+            self.cursor.executescript(schemas.schema(self.db_path))
+            return True
+
+        for table in content:
             try:
-                self.cursor.execute('drop table %s' % (t,))
+                self.cursor.execute('TRUNCATE TABLE %s' % (table,))
             except sqlite3.OperationalError:
                 # skip, can not drop table...
                 continue
-        self.cursor.executescript(skel.schema(self.db_path))
+        return True
 
     def commit(self):
         try:
             return self.connection.commit()
         except sqlite3.OperationalError as err:
+            # TODO: Parse the exception and show it to the user in a suitable form
             print(err)
             lpms.terminate()
-
-    def get_repos(self):
-        self.cursor.execute('''select repo from metadata''')
-        return list(set(self.cursor.fetchall()))
-
-    def get_slot(self, category, name, version, repo=None):
-        if repo:
-            self.cursor.execute('''select version from metadata where repo=(?) and category=(?) and name=(?)''', 
-                    (repo, category, name,))
-        else:
-            self.cursor.execute('''select version from metadata where category=(?) and name=(?)''', 
-                    (category, name,))
-        
-        items = self.cursor.fetchall()
-        for item in items:
-            # first object of the tuple ships data
-            result = pickle.loads(str(item[0]))
-            for slot in result:
-                if version in result[slot]:
-                    return slot
-
-    def add_pkg(self, data, commit=True):
-        def get_slot():
-            self.cursor.execute('''select version from metadata where repo=(?) and category=(?) and name=(?)''', 
-                    (repo, category, name,))
-            
-            result = self.cursor.fetchall()
-            return pickle.loads(str(result[0][0]))
-        
-        def get_options():
-            data = self.cursor.execute('''select options from metadata where repo=(?) and category=(?) and name=(?)''', 
-                    (repo, category, name,))
-            return pickle.loads(str(self.cursor.fetchone()[0]))
-
-        def get_arch():
-            self.cursor.execute('''select arch from metadata  where repo=(?) and category=(?) and name=(?)''',
-                    (repo, category, name,))
-            return pickle.loads(str(self.cursor.fetchone()[0]))
-
-        repo, category, name, version, summary, homepage, _license, src_url, options, slot, arch = data
-        if not self.pkg_is_exists(repo, category, name):
-            version_data = sqlite3.Binary(pickle.dumps({slot: [version]}, 1))
-            options_data = sqlite3.Binary(pickle.dumps({version: options}, 1))
-            arch_data = sqlite3.Binary(pickle.dumps({version: arch}, 1))
-            self.cursor.execute('''insert into metadata values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (
-                repo, category, name, version_data, summary, homepage, _license, src_url, options_data, arch_data))
-        else:
-            current_versions = self.get_version(name, repo, category)
-            if slot in current_versions:
-                current_versions[slot].append(version)
-            else:
-                current_versions.update({slot: [version]})
-            self.cursor.execute('''update metadata set version=(?) where repo=(?) and category=(?) and name=(?)''', (
-                (sqlite3.Binary(pickle.dumps(current_versions, 1)), repo, category, name)))
-            
-            current_options = get_options()
-            if version in current_options:
-                current_options[version] = options
-            else:
-                current_options.update({version: options})
-            self.cursor.execute('''update metadata set options=(?) where repo=(?) and category=(?) and name=(?)''', (
-                (sqlite3.Binary(pickle.dumps(current_options, 1)), repo, category, name)))
-
-            current_arch = get_arch()
-            if arch in current_arch:
-                current_arch[version] = arch
-            else:
-                current_arch.update({version: arch})
-            self.cursor.execute('''update metadata set arch=(?) where repo=(?) and category=(?) and name=(?)''', (
-                sqlite3.Binary(pickle.dumps(current_arch, 1)), repo, category, name))
-
-        if commit:
-            self.commit()
-
-    def pkg_is_exists(self, repo, category, name):
-        for pkg in self.find_pkg(name):
-            if (repo, category, name) == pkg[:-1]:
-                return True
-        return False
-
-    def find_pkg(self, name):
-        self.cursor.execute('''select repo, category, name, version from metadata where name=(?)''', (name,))
-        result = self.cursor.fetchall()
-        if not result:
-            return result
-        return [(repo, category, name, pickle.loads(str(version))) for repo, category, name, version in result]
-    
-    def get_metadata(self, keyword, repo, category, name):
-        self.cursor.execute('''select %s from metadata where repo=(?) and category=(?) and name=(?)''' 
-                % keyword, (repo, category, name,))
-        return self.cursor.fetchone()
-
-    def get_all_names(self, repo=None):
-        if repo is not None:
-            self.cursor.execute('''select repo, category, name from metadata where repo=(?)''', (repo,))
-        else:
-            self.cursor.execute('''select repo, category, name from metadata''')
-        return self.cursor.fetchall()
-
-    def drop(self, rname, category=None, name=None, version=None):
-        # FIXME: Use executescript
-        def drop_others():
-            try:
-                self.cursor.execute('''delete from build_info where repo=(?) and category=(?) and name=(?) and version=(?)''', 
-                        (rname, category, name, version,))
-            except sqlite3.OperationalError:
-                pass
-            self.cursor.execute('''delete from depends where repo=(?) and category=(?) and name=(?) and version=(?)''', 
-                    (rname, category, name, version,))
-
-        if category is None and name is None and version is None:
-            self.cursor.execute('''delete from metadata where repo=(?)''', (rname,))
-            self.cursor.execute('''delete from depends where repo=(?)''', (rname,))
-        else:
-            for pkg in self.find_pkg(name):
-                irepo, icategory, iname, iversion = pkg
-                # FIXME: This is no good!
-                if (icategory, iname) == (category, name):
-                    for key in iversion.keys():
-                        if version in iversion[key]:
-                            iversion[key].remove(version)
-                            if not iversion[key]:
-                                del iversion[key]
-                    
-                    versions = []
-                    map(lambda x: versions.extend(x), iversion.values())
-                    
-                    if len(versions) >= 1:
-                        self.cursor.execute('''update metadata set version=(?) where repo=(?) and category=(?) and name=(?)''', 
-                                (sqlite3.Binary(pickle.dumps(iversion, 1)), rname, category, name,))
-                        drop_others()
-                        break
-                    elif not versions:
-                        self.cursor.execute('''delete from metadata where repo=(?) and category=(?) and name=(?)''', 
-                            (rname, category, name,))
-                        drop_others()
-        self.commit()
-
-    def add_buildinfo(self, data):
-        repo, category, name, version, build_time, hosts, cflags, cxxflags, ldflags, applied, size = data
-        self.cursor.execute('''insert into build_info values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', (
-                repo, category, name, version, build_time, hosts, cflags, cxxflags, ldflags, applied, size))
-        self.commit()
-
-    def drop_buildinfo(self, repo, category, name, version):
-        self.cursor.execute('''delete from build_info where repo=(?) and category=(?) and name=(?) and version=(?)''', 
-                (repo, category, name, version,))
-        self.commit()
-
-    def add_depends(self, data, commit=True):
-        repo_name, category, name, version, build, runtime, postmerge, conflict = data
-        if not self.get_depends(repo_name, category, name, version):
-            self.cursor.execute('''insert into depends values (?, ?, ?, ?, ?, ?, ?, ?)''', 
-                    (repo_name, category, name, version,
-                    sqlite3.Binary(pickle.dumps(build, 1)),
-                    sqlite3.Binary(pickle.dumps(runtime, 1)),
-                    sqlite3.Binary(pickle.dumps(postmerge, 1)),
-                    sqlite3.Binary(pickle.dumps(conflict, 1))
-                    ))
-        if commit:
-            self.commit()
-
-    def get_buildinfo(self, repo, category, name, version):
-        self.cursor.execute('''select * from build_info where repo=(?) and category=(?) and name=(?) and version=(?)''', 
-                (repo, category, name, version,))
-        return self.cursor.fetchone()
-
-    def get_depends(self, repo_name, category, name, version):
-        #FIXME: remove magic numbers
-        self.cursor.execute('''select * from depends where repo=(?) and category=(?) and name=(?) and version=(?)''', 
-            (repo_name, category, name, version,))
-
-        result = {}
-        for deps in self.cursor.fetchall():
-            if deps[6] is None:
-                postmerge = []
-            else:
-                postmerge = pickle.loads(str(deps[6]))
-
-            if deps[7] is None:
-                conflict = []
-            else:
-                conflict = pickle.loads(str(deps[7]))
-
-            result.update({'build': pickle.loads(str(deps[4])),
-                    'runtime': pickle.loads(str(deps[5])),
-                    'postmerge': postmerge,
-                    'conflict': conflict})
-        return result
-
-    def get_options(self, repo, category, name):
-        self.cursor.execute('''select options from metadata where repo=(?) and category=(?) and name=(?)''', 
-                (repo, category, name,))
-
-        result = {}
-        try:
-            data = self.cursor.fetchall()
-            for d in data:
-                result.update(pickle.loads(str(d[0])))
-            return result
-        except TypeError:
-            return
-
-    def get_arch(self, repo, category, name, version=None):
-        self.cursor.execute('''select arch from metadata where repo=(?) and category=(?) and name=(?)''', 
-                (repo, category, name,))
-        try:
-            data = pickle.loads(str(self.cursor.fetchone()[0]))
-            if version:
-                return data[version]
-            return data
-        except TypeError:
-            return
-    
-    def get_version(self, name, repo, category):
-        # FIXME
-        if repo:
-            self.cursor.execute('''select version from metadata where repo=(?) \
-                    and category=(?) and name=(?)''' ,(repo, category, name))
-        else:
-            self.cursor.execute('''select version from metadata where category=(?) \
-                    and name=(?)''', (category, name))
-        
-        results = self.cursor.fetchall()
-        if not results: return
-        versions = {}
-        for result in results:
-            versions.update(pickle.loads(str(result[0])))
-
-        return versions
